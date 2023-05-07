47809bb4953e77ecca80429472ea38793c09f2ab
Fajarwz <fajarwindhuzulfikar@gmail.com>
2023-05-07 11:29:50 +0700

    add login page

---
+++ resources/views/pages/auth/login.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.auth')
+
+@section('body')
+    <section class="bg-gray vh-100">
+        <div class="container h-100 pt-5">
+            <div class="row justify-content-center">
+                <div class="col-12 col-lg-3">
+                    <a href="" class="nav-link mb-5 text-center">
+                        <img class="h-32px" src="{{ url('assets/images/logo.png') }}" alt="Laracuss logo">
+                    </a>
+                    <div class="card mb-5">
+                        <form action="">
+                            <div class="mb-3">
+                                <label for="email" class="form-label">Email</label>
+                                <input type="email" class="form-control" id="email" placeholder="name@example.com" autocomplete="off" autofocus>
+                            </div>
+                            <div class="mb-3">
+                                <label for="password" class="form-label">Password</label>
+                                <div class="input-group">
+                                    <input type="password" class="form-control border-end-0 pe-0 rounded-0 rounded-start"
+                                        id="password" name="password">
+                                    <span class="input-group-text bg-white border-start-0 pe-auto">
+                                        <a href="javascript:;" id="password-toggle">
+                                            <img src="{{ url('assets/images/eye-slash.png') }}" alt="Password toggle" id="password-toggle-img">
+                                        </a>
+                                    </span>
+                                </div>
+                            </div>
+                            <div class="mb-3 d-grid">
+                                <button type="submit" class="btn btn-primary rounded-2">Log in</button>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="text-center">
+                        Don't have an account? <a href="#" class="text-underline"><u>Sign up</u></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 })->name('home');
+
+Route::get('login', function () {
+    return view('pages.auth.login');
+});
