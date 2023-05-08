300c71c4d8413c878e69a25b4511e780e809dd08
Fajarwz <fajarwindhuzulfikar@gmail.com>
2023-05-08 09:20:53 +0700

    add event listener for password toggle

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -39,3 +39,22 @@
         </div>
     </section>
 @endsection
+
+@section('after-script')
+    <script>
+        var isPasswordRevealed = false;
+
+        $('#password-toggle').on('click', function() {
+            isPasswordRevealed = !isPasswordRevealed;
+
+            if (isPasswordRevealed) {
+                $('#password-toggle-img').attr('src', "{{ url('assets/images/eye.png') }}");
+                $('#password').attr('type', 'text');
+            }
+            else {
+                $('#password-toggle-img').attr('src', "{{ url('assets/images/eye-slash.png') }}");
+                $('#password').attr('type', 'password');
+            }
+        })
+    </script>
+@endsection
