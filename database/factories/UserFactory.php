8fb6cfb39143fa98aa3ab6f1ddb651ae602610a6
Fajarwz <fajarwindhuzulfikar@gmail.com>
2023-05-22 08:44:29 +0700

    add name to factory of user picture

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,7 +21,7 @@ class UserFactory extends Factory
             'username' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'picture' => config('app.avatar_generator_url'),
+            'picture' => config('app.avatar_generator_url').'test',
         ];
     }
 
