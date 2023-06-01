83db46e766f4a61f1ecab88172ff180e2c3b4ba6
Fajarwz <fajarwindhuzulfikar@gmail.com>
2023-06-02 06:17:19 +0700

    add softDeletes to Discussion

--- app/Models/Discussion.php
+++ app/Models/Discussion.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Conner\Likeable\Likeable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Discussion extends Model
 {
-    use HasFactory, Likeable;
+    use HasFactory, Likeable, SoftDeletes;
 
     protected $fillable = [
         'user_id',
