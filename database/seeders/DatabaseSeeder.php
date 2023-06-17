8529a07b5b94c617f68dc2df674882a8cf52250e
Fajarwz <fajarwindhuzulfikar@gmail.com>
2023-06-17 18:54:28 +0700

    update user seeder in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,10 +13,13 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
+        $username = 'fajarwz';
 
-        \App\Models\User::factory()->create([
+        \App\Models\User::create([
             'username' => 'fajarwz',
             'email' => 'fajarwz@test.com',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'picture' => config('app.avatar_generator_url').$username,
         ]);
 
         $this->call([
